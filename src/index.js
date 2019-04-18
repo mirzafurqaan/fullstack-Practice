4d47fb23544e467b56e9684a4cefcb4e4a6f1e96
mirzafurqaan <48507334+mirzafurqaan@users.noreply.github.com>
2019-04-18 11:41:36 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+ReactDOM.render(
+  <App initialData={window.initialData} />,
+  document.getElementById('root')
+);
+
+// setTimeout(function (){
+//   ReactDOM.render(
+//     <h2>clear!</h2>,
+//     document.getElementById('root')
+//   );
+// }, 5000);
